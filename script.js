7f19b4037e07c8bd82f7d88c25fa4b3d28132808
Web Design <akahn28@gmail.com>
2025-07-05 22:43:47 -0700

    go

--- script.js
+++ script.js
@@ -381,6 +381,24 @@ if (window.location.pathname === "/past-deliveries") {
       localStorage.setItem("scrollPosition", window.scrollY);
     });
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const pilotRatingsBtn = document.getElementById("pilot-ratings-btn");
+  const customModal = document.getElementById("custom-modal");
+  const modalClose = document.getElementById("custom-modal-close");
+
+  if (pilotRatingsBtn && customModal && modalClose) {
+    pilotRatingsBtn.addEventListener("click", function () {
+      customModal.style.display = "flex";
+    });
+
+    modalClose.addEventListener("click", function () {
+      customModal.style.display = "none";
+    });
+  }
+});
+
+
 }
 
 
